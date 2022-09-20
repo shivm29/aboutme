1840bed0e8ecc26539c91cd330d45017b0c3046c
shivm29 <90485023+shivm29@users.noreply.github.com>
2022-09-20 15:57:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <section className="intro">Hey there! I'm Shivam aka POMTY</section>
+    
+    <div className="sec-1">
+    
+    
+    <div className="bio-container">
+      <h1 className="under-style bio-headings">#Bio</h1>
+      <span className="boldit">2019</span> Completed Class 10th
+      <br />
+      <span className="boldit">2021</span> Completed Class 12th
+      <br />
+      <span className="boldit">Aug 2022</span> Completed 1st Iteration of Data Structures & Algorithms
+      <br />
+      <span className="boldit">Aug 2022</span> Code Help Phase One
+      <br />
+      <span className="boldit">Aug 2022</span> Code Help Phase Two
+      <br />
+      <span className="boldit">Sept 2022</span> Started Web Development
+      <br />
+
+      <h1 className="under-style bio-headings">#I🤍</h1>
+      Spirituality, Football, Anime & Books
+
+      <h1 className="under-style bio-headings">#OnTheWeb</h1>
+      Instagram LinkedIn Github
+
+    
     </div>
-  );
+
+    <div className="main-container">
+      <div className="work-container">
+        <h1>Work</h1>
+        <br></br>
+        Shivam is a second year undergrad student 2025 batch from Medi-Caps University, Indore and a web developer based in Indore with a passion for building digital services/stuff he wants. He has a knack When not online, he loves hanging out and travel else you will see him in Football Ground
+        </div>
+      
+      <div className="heading-container">
+      <h1>Projects</h1>
+      </div>
+
+      <div className="project-container">
+      <div className="projects">
+        <div className="project"><a href="https://shivm29.github.io/skye/">Ecommerce</a></div>
+        <div className="project"><a href="https://vermillion-mermaid-44c887.netlify.app/">Todo List</a></div>
+        <div className="project"><a href="https://shivm29.github.io/linkedinclone/">LinkedIn Clone</a></div>
+        <div className="project">Restronaunt</div>
+      </div>
+      <div className="others">
+        <h1>Code-Help</h1>
+        <br />
+        <div className="others-content">
+          Code-Help is Coding classes conducted by Shivam. So far 2 phases of Code-help are completed. In these classes Shivam teach c++ to his students. Despite of only 2 phases of code-help it has grown exponentially with 5x more students and 11x more earnings in phase 2 if compared to phase 1.
+          Next thing shivam's going to teach in code help is Data Structures and Algorithm.
+          <br />
+          Code-Help Phase 3 is Comming Soon..
+        </div>
+      </div>
+      </div>
+      
+    </div>
+    </div>
+
+    </>
+  )
 }
 
 export default App;
